ce532922855d1df4feb23067e7b2d8f08105e4d9
akoonth1 <a.koontharana@gmail.com>
2024-09-18 19:46:25 -0400

    update

--- A_script/script.js
+++ A_script/script.js
@@ -6,107 +6,7 @@
 
 //Due Date: 2024-09-18
 
-// // The provided course information.
-// const CourseInfo = {
-//     id: 451,
-//     name: "Introduction to JavaScript"
-//   };
-  
-//   // The provided assignment group.
-//   const AssignmentGroup = {
-//     id: 12345,
-//     name: "Fundamentals of JavaScript",
-//     course_id: 451,
-//     group_weight: 25,
-//     assignments: [
-//       {
-//         id: 1,
-//         name: "Declare a Variable",
-//         due_at: "2023-01-25",
-//         points_possible: 50
-//       },
-//       {
-//         id: 2,
-//         name: "Write a Function",
-//         due_at: "2023-02-27",
-//         points_possible: 150
-//       },
-//       {
-//         id: 3,
-//         name: "Code the World",
-//         due_at: "3156-11-15",
-//         points_possible: 500
-//       }
-//     ]
-//   };
-  
-//   // The provided learner submission data.
-//   const LearnerSubmissions = [
-//     {
-//       learner_id: 125,
-//       assignment_id: 1,
-//       submission: {
-//         submitted_at: "2023-01-25",
-//         score: 47
-//       }
-//     },
-//     {
-//       learner_id: 125,
-//       assignment_id: 2,
-//       submission: {
-//         submitted_at: "2023-02-12",
-//         score: 150
-//       }
-//     },
-//     {
-//       learner_id: 125,
-//       assignment_id: 3,
-//       submission: {
-//         submitted_at: "2023-01-25",
-//         score: 400
-//       }
-//     },
-//     {
-//       learner_id: 132,
-//       assignment_id: 1,
-//       submission: {
-//         submitted_at: "2023-01-24",
-//         score: 39
-//       }
-//     },
-//     {
-//       learner_id: 132,
-//       assignment_id: 2,
-//       submission: {
-//         submitted_at: "2023-03-07",
-//         score: 140
-//       }
-//     }
-//   ];
-  
-//   function getLearnerData(course, ag, submissions) {
-//     // here, we would process this data to achieve the desired result.
-//     const result = [
-//       {
-//         id: 125,
-//         avg: 0.985, // (47 + 150) / (50 + 150)
-//         1: 0.94, // 47 / 50
-//         2: 1.0 // 150 / 150
-//       },
-//       {
-//         id: 132,
-//         avg: 0.82, // (39 + 125) / (50 + 150)
-//         1: 0.78, // 39 / 50
-//         2: 0.833 // late: (140 - 15) / 150
-//       }
-//     ];
-  
-//     return result;
-//   }
-  
-//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-//   console.log(result);
+
 
 /*
 Requirements:
@@ -337,6 +237,17 @@ switch (CourseInfo.id) {
 
 const result =[];
 
+// try {
+//   if (typeof()!== Number) throw "Invalid grade";
+  
+// } catch (error) {
+
+//   console.log(error)
+
+// }
+
+
+  
 let test = LearnerSubmissions1(LearnerSubmission);
 u_id = test;
 
@@ -366,6 +277,22 @@ for (let i = 0; i < u_id.length; i++) {
             if(el.id === assignment_id && current_date > dateformat(el.due_at))
 
             {
+
+              
+                try {
+                  if (typeof(el.points_possible) !== "number") throw "Invalid grade";
+                  if (el.points_possible <= 0) throw "Invalid points";
+                  
+                } catch (error) {
+
+                  console.log(error)
+                 
+
+                }
+            
+
+
+
 
                 let test_score;
                 if (is_not_late(dateformat(el.due_at),dateformat(element[2])))
@@ -462,3 +389,47 @@ console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions));
 //   ];
 
 // console.log(result2)
+
+
+
+// try {
+//   if (typeof(x)!== Number) throw "Invalid grade";
+  
+// } catch (error) {
+
+//   console.log(error)
+
+// }
+  
+
+
+
+
+// try {
+//   if (typeof(x) == 0) throw "Invalid grade";
+  
+// } catch (error) {
+
+//   console.log(error)
+
+// }
+  
+
+
+
+
+// function myFunction() {
+//   const message = document.getElementById("p01");
+//   message.innerHTML = "";
+//   let x = document.getElementById("demo").value;
+//   try { 
+//     if(x.trim() == "")  throw "empty";
+//     if(isNaN(x)) throw "not a number";
+//     x = Number(x);
+//     if(x < 5)  throw "too low";
+//     if(x > 10)   throw "too high";
+//   }
+//   catch(err) {
+//     message.innerHTML = "Input is " + err;
+//   }
+// }
